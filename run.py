058a49896887ae8fcd9d426aa80f504a6b0395d2
darren robinson <darrenrobinsonpt@gmail.com>
2024-07-08 19:51:20 +0000

    Using post method

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -26,11 +26,13 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>" 
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
     return render_template("contact.html", page_title="Contact")
 
 
